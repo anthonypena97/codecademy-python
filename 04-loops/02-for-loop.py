a996ad862149d93ddace3bfbe34fd319ba4d003d
Anthony Pena <apena5@alumni.jh.edu>
2022-10-25 23:16:50 -0600

    feat: chapter 4 coursework

---
+++ 04-loops/02-for-loop.py
@@ -0,0 +1,8 @@
+board_games = ["Settlers of Catan", "Carcassone", "Power Grid", "Agricola", "Scrabble"]
+
+sport_games = ["football", "hockey", "baseball", "cricket"]
+
+for game in board_games:
+  print(game)
+
+for games in sport_games: print(games)
