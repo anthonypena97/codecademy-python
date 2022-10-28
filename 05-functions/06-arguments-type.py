24ec1ffc7c65438c721eb113152b245b70439314
Anthony Pena <apena5@alumni.jh.edu>
2022-10-27 22:40:54 -0600

    feat: chapter 5 - course work

---
+++ 05-functions/06-arguments-type.py
@@ -0,0 +1,10 @@
+# Write your code below:
+def trip_planner(first_destination, second_destination, final_destination = "Codecademy HQ"):
+  print("Here is what your trip will look like!")
+  print("First, we will stop in " + first_destination + ", then " + second_destination + ", and lastly " + final_destination)
+
+# Keyword Arguments
+# trip_planner(first_destination ="Iceland", final_destination = "Germany", second_destination = "India")
+
+# Default arguments
+trip_planner("Brooklyn", "Queens")
